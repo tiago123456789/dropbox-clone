58dc2ef10ba593153070060a8d0116eb98145f73
Tiago Rosa da costa <tiagorosadacost@gmail.com>
2020-04-09 22:29:54 -0300

    Init project dropbox clone

--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
 import React from "react";
 import ReactDom from "react-dom";
+import * as firebase from "firebase"
 import CONSTANTS from "./constantes/App";
-console.log(CONSTANTS);
 
+ const firebaseConfig = {
+    apiKey: firebase_apiKey,
+    authDomain: firebase_authDomain,
+    databaseURL: firebase_databaseURL,
+    projectId: firebase_projectId,
+    storageBucket: firebase_storageBucket,
+    messagingSenderId: firebase_messagingSenderId,
+    appId: firebase_appId,
+    measurementId: firebase_measurementId
+  };
+
+  const app = firebase.initializeApp(firebaseConfig)
+  app.database()
+    .ref("peoples")
+    .push({ name: "Another user", email: "anotheruser@gmail.com" })
 ReactDom.render((<p>Text</p>), document.getElementById("root"));
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 var path = require('path');
 var webpack = require('webpack');
+require("dotenv").config();
 
 module.exports = {
     entry: './src/index.js',
@@ -18,6 +19,13 @@ module.exports = {
                 test: /\.js$/,
                 use: 'babel-loader',
                 exclude: /node_modules/
+            },
+            {
+                test: /\.css$/,
+                use: [
+                  'style-loader',
+                  'css-loader'
+                ]
             }
         ]
     },
@@ -27,6 +35,14 @@ module.exports = {
     plugins: [
         new webpack.DefinePlugin({
             ENV: JSON.stringify(process.env.ENVIRONMENT),
+            firebase_apiKey: JSON.stringify(process.env.firebase_apiKey),
+            firebase_authDomain: JSON.stringify(process.env.firebase_authDomain),
+            firebase_databaseURL: JSON.stringify(process.env.firebase_databaseURL),
+            firebase_projectId: JSON.stringify(process.env.firebase_projectId),
+            firebase_storageBucket: JSON.stringify(process.env.firebase_storageBucket),
+            firebase_messagingSenderId: JSON.stringify(process.env.firebase_messagingSenderId),
+            firebase_appId: JSON.stringify(process.env.firebase_appId),
+            firebase_measurementId: JSON.stringify(process.env.firebase_measurementId)
         })
     ],
 };
